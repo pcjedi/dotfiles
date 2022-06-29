ac0dacaeef4a842b1303caafff3249ccae2285c5
Georg Wendorf <22711712+pcjedi@users.noreply.github.com>
2022-06-29 12:44:15 +0200

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-if [[ $(awk -F= '/^ID/{print $2}' /etc/os-release) == "alpine" ]]; then
-    apk add openconnect
-fi
-
-if [[ $(awk -F= '/^ID/{print $2}' /etc/os-release) == "debian" ]]; then
-    apt-get update
-    apt-get -y install openconnect
-fi
