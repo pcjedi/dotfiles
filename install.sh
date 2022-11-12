901195642e30a5fe739ab44d2ebab73ac805e90d
Georg Wendorf <22711712+pcjedi@users.noreply.github.com>
2022-11-12 20:04:59 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,26 @@
-if [[ $(awk -F= '/^ID/{print $2}' /etc/os-release) == "alpine" ]]; then
-    sudo apk add openconnect
+# Alpine
+if command -v apk &> /dev/null; then
+    if ! command -v openconnect &> /dev/null; then
+        sudo apk add openconnect
+    fi
 fi
 
-if [[ $(awk -F= '/^ID/{print $2}' /etc/os-release) == "debian" ]]; then
-    sudo apt-get update
-    sudo apt-get -y install openconnect
+#Debian/Ubuntu
+if command -v apt-get &> /dev/null; then
+    if ! command -v openconnect &> /dev/null; then
+        sudo apt-get update
+        sudo apt-get -y upgrade
+        sudo apt-get -y install openconnect --no-install-recommends
+        sudo rm -rf /var/lib/apt/lists/*
+    fi
+    if ! [ -d "/opt/oracle" ]; then
+        sudo apt-get update
+        sudo apt-get -y upgrade
+        sudo apt-get -y install libaio1 wget unzip --no-install-recommends
+        sudo mkdir -p /opt/oracle
+        sudo wget --directory-prefix=/tmp/ https://download.oracle.com/otn_software/linux/instantclient/instantclient-basiclite-linuxx64.zip
+        sudo unzip /tmp/instantclient-basiclite-linuxx64.zip -d /opt/oracle/
+        sudo ln -s /opt/oracle/* /opt/oracle/instantclient
+        sudo rm -rf /var/lib/apt/lists/* /tmp/*
+    fi
 fi
